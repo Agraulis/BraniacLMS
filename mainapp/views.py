e3bd1c0facd2ba4398f3a0550f237e17d9f585c7
Denis Roshchupkin <denis_roshypkin@mail.ru>
2022-11-14 22:16:32 +0300

    homework 2 added

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+"""
+    path('', views.HelloWorldView.as_view()),
+    path('news/', views.NewsPageView.as_view()),
+    path('courses/', views.CoursesPageView.as_view()),
+    path('contacts/', views.ContactsPageView.as_view()),
+    path('doc_style/', views.DocSitePageView.as_view()),
+    path('login/', views.LoginPageView.as_view()),
+"""
+
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
