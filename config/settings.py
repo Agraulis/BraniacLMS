ff36791f50e91108973b4c31d75ef2f97e012d89
Agraulis <72881808+Agraulis@users.noreply.github.com>
2022-12-07 20:29:01 +0300

    Lesson8 (#9)
    * lesson_8 i18n
    
    * lesson_8 Tests
    
    * new migration

--- config/settings.py
+++ config/settings.py
@@ -211,6 +211,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -229,3 +230,7 @@ CACHES = {
         },
     }
 }
+
+LOCALE_PATHS = [BASE_DIR / "locale"]
+
+SELENIUM_DRIVER_PATH_FF = BASE_DIR / "var" / "selenium" / "geckodriver"
